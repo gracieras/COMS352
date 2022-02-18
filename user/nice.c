cfebbee8fbc395e3e9308a6e2fa5aae4fc26c49c
RedBlueDevil <noahtang1997@yahoo.com>
2022-02-18 16:55:35 -0600

    Update nice.c

--- user/nice.c
+++ user/nice.c
@@ -1,13 +1,13 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
+#include "kernel/syscall.h"
 
 int
 main(int argc, char *argv[])
 {
     int nicevalue = atoi(argv[1]);
-    // nice();
-
+    
     sys_nice(nicevalue);
 
     char **arr = (char **)&argv[3];
