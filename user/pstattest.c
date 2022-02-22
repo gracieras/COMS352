fabb581eb08604975f15179dad048eb7f4b01e90
ger <ger@iastate.edu>
2022-02-21 21:56:43 -0600

    documentation

--- user/pstattest.c
+++ user/pstattest.c
@@ -6,10 +6,11 @@
 int
 main(void)
 {
-  struct pstat stats;
-  getpstat(&stats);
+  struct pstat stats;         //creating a struct pstat
+  getpstat(&stats);           //call the syscall and pass in the just created struct by pointer
+
   // print the arrays in stats
-  for(int i=0;i<NPROC;i++){
+  for(int i=0;i<NPROC;i++){   //loop through every i
     if(stats.inuse[i]==1){    //if stats is in use, print the arrays
       printf("inuse: ");
       printf("%d \n",stats.inuse[i]);
