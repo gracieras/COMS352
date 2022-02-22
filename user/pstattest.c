59c6b5c0a4bbf557c719a915d40dc23271fb9842
RedBlueDevil <noahtang1997@yahoo.com>
2022-02-21 21:15:05 -0600

    Update pstattest.c

--- user/pstattest.c
+++ user/pstattest.c
@@ -12,13 +12,13 @@ main(void)
   for(int i=0;i<NPROC;i++){
     if(stats.inuse[i]==1){
       printf("inuse");
-      printf("%d\n",stats.inuse[i]);
+      printf("%d \n",stats.inuse[i]);
 
       printf("pid");
-      printf("%d\n",stats.pid[i]);
+      printf("%d \n",stats.pid[i]);
 
       printf("nice");
-      printf("%d\n",stats.nice[i]);
+      printf("%d \n",stats.nice[i]);
     }
   }
   exit(0);
