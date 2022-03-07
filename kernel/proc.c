eee31972b3467e11fbc3bbc63c57ff911010e9ef
RedBlueDevil <noahtang1997@yahoo.com>
2022-03-06 18:03:43 -0600

    updated scheduler_rr

--- kernel/proc.c
+++ kernel/proc.c
@@ -614,29 +614,41 @@ scheduler_rr(void)
   struct proc *p;
   struct cpu *c = mycpu();
   c->proc = 0;
-
+  int queueid;
   queue = newqueue();
   
   for(;;){
     // Avoid deadlock by ensuring that devices can interrupt.
     intr_on();
-
-    for(p = proc; p < &proc[NPROC]; p++) {
+    while (!isempty(queue))
+    {
+      queueid = dequeue(queue);
+      // p = qtable[queueid].pass;
+      // uint64 pindex = p - proc;
+      proc[queueid].state = RUNNING;
+      p = proc;
       acquire(&p->lock);
-      if(p->state == RUNNABLE) {
-        // Switch to chosen process.  It is the process's job
-        // to release its lock and then reacquire it
-        // before jumping back to us.
-        p->state = RUNNING;
-        c->proc = p;
-        swtch(&c->context, &p->context);
-
-        // Process is done running for now.
-        // It should have changed its p->state before coming back.
-        c->proc = 0;
-      }
+      c->proc = p;
+      swtch(&c->context, &proc[queueid].context);
+      c->proc = 0;
       release(&p->lock);
     }
+    // for(p = proc; p < &proc[NPROC]; p++) {
+    //   acquire(&p->lock);
+    //   if(p->state == RUNNABLE) {
+    //     // Switch to chosen process.  It is the process's job
+    //     // to release its lock and then reacquire it
+    //     // before jumping back to us.
+    //     p->state = RUNNING;
+    //     c->proc = p;
+    //     swtch(&c->context, &p->context);
+
+    //     // Process is done running for now.
+    //     // It should have changed its p->state before coming back.
+    //     c->proc = 0;
+    //   }
+    //   release(&p->lock);
+    // }
   }
 }
 
