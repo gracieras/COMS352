89f8d984117b6ab23faad02be3dafdb517fdd161
ger <ger@iastate.edu>
2022-03-07 00:20:13 -0600

    define stride

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -109,6 +109,22 @@ sys_nice(void)
   
   myproc()->nicevalue = nicevalue; //sets the nice value
 
+  int stride;
+  int tickets;
+
+  static const int nice_to_tickets[40] = {
+    /* -20 */ 88761, 71755, 56483, 46273, 36291,
+    /* -15 */ 29154, 23254, 18705, 14949, 11916,
+    /* -10 */ 9548, 7620, 6100, 4904, 3906,
+    /* -5 */ 3121, 2501, 1991, 1586, 1277,
+    /* 0 */ 1024, 820, 655, 526, 423,
+    /* 5 */ 335, 272, 215, 172, 137,
+    /* 10 */ 110, 87, 70, 56, 45,
+    /* 15 */ 36, 29, 23, 18, 15,
+  };
+  nice_to_tickets[40] = (myproc()->nicevalue);
+  stride = 1000000 / tickets; // stride is 1 million / tickets
+
   return 0; // returns 0 if it succeeds
 }
 
