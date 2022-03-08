7eb66d5130efcd4b1a15c2e738a05eb8b7cfc8bf
RedBlueDevil <noahtang1997@yahoo.com>
2022-03-07 19:04:27 -0600

    3.3.2 allocproc

--- kernel/proc.c
+++ kernel/proc.c
@@ -280,7 +280,7 @@ found:
   memset(&p->context, 0, sizeof(p->context));
   p->context.ra = (uint64)forkret;
   p->context.sp = p->kstack + PGSIZE;
-
+  p->pass = firstkey(queue) + nice(p->nicevalue);
   return p;
 }
 
