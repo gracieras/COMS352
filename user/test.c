d27b21c7da5c1fe3673a9c38f05207ee2ba596f4
ger <ger@iastate.edu>
2022-03-07 21:44:14 -0600

    update user/test.c

--- user/test.c
+++ user/test.c
@@ -5,19 +5,21 @@
 int
 main(void)
 {
-    int c1_pid, c2_pid;     //define children to fork into
+    while(1){
+        int c1_pid, c2_pid;     //define children to fork into
 
-    (c1_pid = fork()) && (c2_pid = fork()); // Creates two children
+        (c1_pid = fork()) && (c2_pid = fork()); // Creates two children
 
-    if (c1_pid == 0) {
-        /* Child 1 code goes here */
-        nice(14); //sets the nice value to 14
+        if (c1_pid == 0) {
+            /* Child 1 code goes here */
+            nice(14); //sets the nice value to 14
         
-    } else if (c2_pid == 0) {
-        /* Child 2 code goes here */
-        nice(17); //sets the nice value to 17
-        
-    } else {
-        /* Parent code goes here */
+        } else if (c2_pid == 0) {
+            /* Child 2 code goes here */
+            nice(17); //sets the nice value to 17
+
+        } else {
+            /* Parent code goes here */
+        }
     }
 }
